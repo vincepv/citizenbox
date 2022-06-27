b087544e0bca2311b512c59f9f2ec12eb7596d40
vpv <vincentpaulvictor@gmail.com>
2022-06-27 18:35:30 +0200

    Login page

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,43 @@
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
+import Logo from "@/components/Logo.vue";
+import TheMenu from "@/components/TheMenu.vue";
+import TheLogin from "@/components/TheLogin.vue";
 
 
-import Logo from '@/components/Logo.vue'
-import Menu from '@/components/Menu.vue'
+const isLog = true;
+
 </script>
 
 <template>
-  <div class="row">
-  <header class="col-2 bg-light ">
-    <div class="m-2">
-      <div>
-        <Logo />
-        <Menu/>
+
+  <div v-if="isLog" class="row">
+    <header class="col-2 bg-light">
+      <div class="m-2">
+        <div>
+          <Logo />
+          <TheMenu />
+        </div>
       </div>
+    </header>
+
+    <div class="col-10">
+      <RouterView />
     </div>
-  </header>
-
-  <div class="col-10">
-    <RouterView />
   </div>
 
+  <div v-else>
+    <TheLogin />
   </div>
+
 </template>
 
+
+
 <style scoped>
-
-
 header {
   flex-grow: 1;
   min-height: 100vh;
 }
-
-
-
 </style>
 
---
+++ src/components/TheLogin.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="text-center">
+      <main class="form-signin">
+        <form>
+          <img id="logo" class="img-fluid m-4" src="../assets/logo.png" >
+    
+          <div class="form-floating ">
+            <input type="user" class="form-control" id="floatingInput" placeholder="user">
+            <label for="floatingInput">User</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
+          <button class="w-100 btn btn-lg btn-dark" type="submit">Log in</button> 
+        </form>
+      </main>
+      <footer class="pt-3 mt-4 text-muted fw-lighter d-flex justify-content-center">
+        &copy; DigitaleBox. All Rights Reserved.
+      </footer>
+  </div>
+
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+#logo {
+  width : 100px;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.form-signin .checkbox {
+  font-weight: 400;
+}
+
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+
+.form-signin input[type="user"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
