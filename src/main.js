5d861ca1b6456a235ab4580da8e1f37d4d1d9e22
vpv <vincentpaulvictor@gmail.com>
2022-06-28 16:38:30 +0200

    remove pinia

--- src/main.js
+++ src/main.js
@@ -1,19 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// state management
-import { createPinia } from 'pinia'
 
 // load bootstrap for the whole project
 import 'bootstrap/dist/css/bootstrap.css'
 
 
-
-const pinia = createPinia()
-
 const app = createApp(App)
-
 app.use(router)
-app.use(pinia)
 app.mount('#app')
 
